debb7b4da6a97a5aad20183740a8624b2832462f
Pavel Zahynailo <pzag97@gmail.com>
2020-03-04 15:09:05 +0200

    Removed unused imports

--- src/app/pay-loans/pay-loans.component.ts
+++ src/app/pay-loans/pay-loans.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pay-loans',
   templateUrl: './pay-loans.component.html',
   styleUrls: ['./pay-loans.component.scss']
 })
-export class PayLoansComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class PayLoansComponent {
 }
