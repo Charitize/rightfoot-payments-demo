ce241818901a35de1a75cc4de44aaa7b320a100c
Pavel Zahynailo <pzahynailo@lohika.com>
2020-03-06 19:07:49 +0200

    updated todo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { Environment, PlaidEnvironment } from './environment.interface';
 
 export const environment: Environment = {
   production: true,
-  // TODO: change to real API URL.
+  // TODO(RF-363) Change to real API URL.
   apiUrl: 'localhost:9000',
   plaid: {
     environment: PlaidEnvironment.PRODUCTION,
